4493c4c7aed6bb13092b153c35f6bf0092513d9a
bad4iz <bad4iz@gmail.com>
2017-03-14 20:19:09 +0400

    init rivg 2

--- RivgRequest.php
+++ RivgRequest.php
@@ -11,5 +11,31 @@ namespace Rivg;
 
 class RivgRequest
 {
+    protected $_url;
+    protected $_followlocation;
+    protected $_timeout;
+    protected $_post;
+
+    public function __construct($url, $timeOut = 1000)
+    {
+        $this->_url = $url;
+        $this->_timeout = $timeOut;
+
+    }
+
+    public function createCurl($url = 'nul')
+    {
+        if($url != 'nul'){
+            $this->_url = $url;
+        }
+        $ch = curl_init($url);
+        $a = curl_exec($ch); // выполняем запрос curl - обращаемся к сервера php.su
+        curl_close($ch);
+        echo $a;
+    }
+
 
 }
+
+$rivg = new RivgRequest("http://api.vk.com/method/board.getComments?group_id=99828403&topic_id=32625403&count=10");
+$rivg->createCurl();
